38eaf951e96bfb9fec036f5835514b5bd4a44bbd
Vincent <55402333+vincent5682@users.noreply.github.com>
2022-01-29 13:20:41 +0100

    Add files via upload

---
+++ CBot/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CBot
+{
+    public class Position
+    {
+        public decimal Amount { get; set; }
+        public decimal Price { get; set; }
+        public string Symbol { get; set; }
+        public string Status { get; set; }
+    }
+}
